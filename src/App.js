0992255f31b0f66d8efce713eed559fee5bd7251
Kim Chen <chenyanliang19910101@gmail.com>
2018-02-28 23:53:05 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
@@ -13,13 +12,6 @@ class App extends Component {
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
             <Form />
             <PostsView />
           </div>
